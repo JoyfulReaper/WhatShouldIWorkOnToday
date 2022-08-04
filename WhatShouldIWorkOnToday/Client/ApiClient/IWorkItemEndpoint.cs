728f5690df03cc36ebfebe4abbd2b8658dbfd253
Kyle Givler <xvegan88x@gmail.com>
2022-08-04 18:56:36 -0400

    Don't Make API calls directly from the Components

---
+++ WhatShouldIWorkOnToday/Client/ApiClient/IWorkItemEndpoint.cs
@@ -0,0 +1,11 @@
+﻿using WhatShouldIWorkOnToday.Client.Models;
+
+namespace WhatShouldIWorkOnToday.Client.ApiClient;
+public interface IWorkItemEndpoint
+{
+    Task DeleteAsync(int Id);
+    Task<List<WorkItem>> GetAllAsync();
+    Task<WorkItem> GetAsync(int id);
+    Task<WorkItem> PostAsync(WorkItem workItem);
+    Task PutAsync(WorkItem workItem);
+}
---
+++ WhatShouldIWorkOnToday/Client/ApiClient/WorkItemEndpoint.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Http.Json;
+using WhatShouldIWorkOnToday.Client.Models;
+using WhatShouldIWorkOnToday.Client.Pages.WorkItems;
+
+namespace WhatShouldIWorkOnToday.Client.ApiClient;
+
+public class WorkItemEndpoint : IWorkItemEndpoint
+{
+    private readonly HttpClient _httpClient;
+
+    public WorkItemEndpoint(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<WorkItem>> GetAllAsync()
+    {
+        var workItems = await _httpClient.GetFromJsonAsync<List<WorkItem>>("api/WorkItem");
+        if (workItems is null)
+        {
+            throw new Exception("Failed to de-serialize list of work items.");
+        }
+
+        return workItems;
+    }
+
+    public async Task<WorkItem> GetAsync(int id)
+    {
+        var workItem = await _httpClient.GetFromJsonAsync<WorkItem>($"api/WorkItem/{id}");
+        if (workItem is null)
+        {
+            throw new Exception("Failed to de-serialize work item.");
+        }
+
+        return workItem;
+    }
+
+    public async Task<WorkItem> PostAsync(WorkItem workItem)
+    {
+        using var response = await _httpClient.PostAsJsonAsync("api/WorkItem", workItem);
+        CheckResponse(response);
+
+        var workItemResp = await response.Content.ReadFromJsonAsync<WorkItem>();
+        if (workItemResp is null)
+        {
+            throw new Exception("Failed to de-serialize work item.");
+        }
+
+        return workItemResp;
+    }
+
+    public async Task PutAsync(WorkItem workItem)
+    {
+        using var response = await _httpClient.PutAsJsonAsync($"api/WorkItem/{workItem.WorkItemId}", workItem);
+        CheckResponse(response);
+    }
+
+    public async Task DeleteAsync(int Id)
+    {
+        using var response = await _httpClient.DeleteAsync($"/api/WorkItem/{Id}");
+        CheckResponse(response);
+    }
+
+    private void CheckResponse(HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"API Response was not successful: {response.StatusCode}");
+        }
+    }
+}
