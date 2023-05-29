2e02da3ebfa6b3597ed927f6aca6b9b7e8c9e2d1
Kyle Givler <xvegan88x@gmail.com>
2023-05-29 12:27:16 -0400

    Re-org... and add giraffe

--- WhatShouldIWorkOnToday/Server/DataAccess/Interfaces/ICurrentSequenceNumberData.cs
+++
@@ -1,9 +0,0 @@
-﻿using WhatShouldIWorkOnToday.Server.Models;
-
-namespace WhatShouldIWorkOnToday.Server.DataAccess.Interfaces;
-public interface ICurrentSequenceNumberData
-{
-    Task<CurrentSequenceNumber> GetAsync();
-    Task<int> GetMaxSequenceNumber();
-    Task UpdateAsync(CurrentSequenceNumber currentSequenceNumber);
-}
