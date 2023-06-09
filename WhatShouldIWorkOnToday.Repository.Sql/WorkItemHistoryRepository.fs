66213b80b138336a4ebd42862ba306263c8a116f
Kyle Givler <xvegan88x@gmail.com>
2023-06-09 17:51:53 -0400

    I think the last repo is done now!

--- WhatShouldIWorkOnToday.Repository.Sql/WorkItemHistoryRepository.fs
+++ WhatShouldIWorkOnToday.Repository.Sql/WorkItemHistoryRepository.fs
@@ -5,13 +5,32 @@ open WhatShouldIWorkOnToday.Repository.Sql.Mapping
 open WhatShouldIWorkOnToday.Repository
 open Microsoft.EntityFrameworkCore
 open System.Linq
+open WhatShouldIWorkOnToday.Models
 
 
-let getWorkItemHistory (context : SqlDbContext) (workItemHistoryId: int) =
+let getWorkItemHistory (context : SqlDbContext) (workItemHistoryId : int) =
     async {
-        let! entity = context.WorkItemHistories.AsNoTracking().Where(fun wih -> wih.WorkItemHistoryId = workItemHistoryId).SingleOrDefaultAsync() 
-                      |> Async.AwaitTask
+        let! entity = context.WorkItemHistories.AsNoTracking()
+                                               .Where(fun wih -> wih.WorkItemHistoryId = workItemHistoryId)
+                                               .SingleOrDefaultAsync() |> Async.AwaitTask
+                      
         return entity |> Option.ofObj |> Option.map WorkItemHistory.toModel
+    }
+
+let getWorkItemHistoryByWorkItemId (context : SqlDbContext) (workItemId : int) =
+    async {
+        let! entities = context.WorkItemHistories.AsNoTracking()
+                                                 .Where(fun wih -> wih.WorkItemId = workItemId)
+                                                 .ToListAsync() |> Async.AwaitTask
+
+        return entities |> List.ofSeq |> List.map WorkItemHistory.toModel
+    }
+
+let saveWorkItemHistory (context : SqlDbContext) (workItemHistory : WorkItemHistory.WorkItemHistory) =
+    async {
+        context.WorkItemHistories.Add(workItemHistory |> WorkItemHistory.toEntity) |> ignore
+        context.SaveChangesAsync() |> Async.AwaitTask |> ignore
+        return getWorkItemHistory context workItemHistory.WorkItemHistoryId |> Async.RunSynchronously
     }
 
 type SqlWorkItemHistoryRepository(context : SqlDbContext) =
@@ -19,7 +38,7 @@ type SqlWorkItemHistoryRepository(context : SqlDbContext) =
         member this.Get(workItemHistoryId: int): Async<WhatShouldIWorkOnToday.Models.WorkItemHistory.WorkItemHistory option> = 
             getWorkItemHistory context workItemHistoryId
         member this.GetByWorkItemId(workItemId: int): Async<WhatShouldIWorkOnToday.Models.WorkItemHistory.WorkItemHistory list> = 
-            raise (System.NotImplementedException())
+            getWorkItemHistoryByWorkItemId context workItemId
         member this.Save(workItemHistory: WhatShouldIWorkOnToday.Models.WorkItemHistory.WorkItemHistory): Async<WhatShouldIWorkOnToday.Models.WorkItemHistory.WorkItemHistory option> = 
             raise (System.NotImplementedException())
         
