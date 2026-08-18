db9e33eec9fec866a306f2749fc2892a4ed12d1c
Kyle Givler <6787154+JoyfulReaper@users.noreply.github.com>
2026-08-18 11:45:25 -0400

    Correct migrated todo defaults

---
+++ WhatShouldIWorkOnToday/Migrations/20260818154425_FixTodoEnergyEffortDefaults.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhatShouldIWorkOnToday.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTodoEnergyEffortDefaults : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                UPDATE TodoItems
+                                SET Energy = 1,
+                                    Effort = 1
+                                WHERE Energy = 0
+                                  AND Effort = 0;
+                                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
